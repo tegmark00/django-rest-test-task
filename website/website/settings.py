4714f13c88965c4cbd58d2c0c8860face3c059a7
Mark Bobkov <mark.bobkov.13@gmail.com>
2021-02-10 11:47:44 +0200

    Added nginx, docker, gunicorn

--- website/website/settings.py
+++ website/website/settings.py
@@ -99,7 +99,7 @@ DATABASES = {
         'USER': os.getenv('POSTGRES_USER', 'postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
         'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
-        'PORT': os.getenv('POSTGRES_HOST', '5430'),
+        'PORT': os.getenv('POSTGRES_PORT', '5430'),
     }
 }
 
